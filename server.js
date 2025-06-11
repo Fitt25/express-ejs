d81602f52d567d0c218e73670053235070d2d5d1
Linda Fitt <162840391+Fitt25@users.noreply.github.com>
2025-06-11 18:13:14 +0000

    Adds routes and images

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ var data = require('./test.json');
 
 //Create server, deal with requests/responses
 
+const recipeRoutes = require('./routes/recipes');
+app.use('/recipes', recipeRoutes);
+
 //home URL
 app.get('/',(req,res) =>{
   let title = "Home Page: Horses";
