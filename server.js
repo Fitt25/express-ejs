64e6ef28f50927bed9aa3d939d51e439dbacaa4a
Linda Fitt <162840391+Fitt25@users.noreply.github.com>
2025-06-11 17:04:00 +0000

    Adds mongoose connection and new seed data

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 //include Express
 const express = require('express');
 
+//includes .env file for credentials
+require('dotenv').config();
+
+//manages database connectivity
+require('./models/mongoose');
+
 //set port
 const port = 3000;
 
